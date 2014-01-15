931eefdc0aaf80c803ff28466a85fadbdf1f5614
Richa <bhardwaj.richa17@gmail.com>
2014-01-16 00:24:47 +0530

    added user model and sessions controller

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ActiveRecord::Base
+  include ActiveModel::SecurePassword
+  has_secure_password
+
+  ## CONSTANTS ##
+
+
+  ## VALIDATIONS ##
+  validates :name, :email, :password_digest, :mobile, presence: true
+  validates :email, uniqueness: { allow_blank: true }
+  validates :email, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/, allow_blank: true }
+  validates :mobile, format: { with: /\A[8-9]\d{7}\z/, allow_blank: true }
+
+  ## ASSOCIATIONS ##
+
+
+  ## CALLBACKS ##
+
+
+  ## INSTANCE METHODS ##
+
+
+  ## CLASS METHODS ##
+
+
+end
---
+++ db/migrate/20140115174034_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :mobile
+      t.string :password_digest
+      t.boolean :is_admin, :default => 0
+      t.decimal :total_credits, :default => 0.00, :precision => 2, :scale => 2
+      t.decimal :availed_credits, :default => 0.00, :precision => 2, :scale => 2
+      t.string :auth_key
+      t.timestamps
+    end
+  end
+end
