1f9e9fcc9c75586ce8a90aabd283c61efcbf09b1
Richa <bhardwaj.richa17@gmail.com>
2014-01-16 00:40:40 +0530

    added admin login logic

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,2 +1,17 @@
 class Admin::SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.where(:email => params[:email]).first
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_root_path
+    else
+      flash[:error] = "Incorrect username/password combination."
+      render :new
+    end
+  end
+
 end
