1a9e62d5e9e397ebe85d6689422ed64e8a850745
Richa <bhardwaj.richa17@gmail.com>
2014-02-27 23:56:10 +0530

    fixed add credits issue

--- app/controllers/api/credits_controller.rb
+++ app/controllers/api/credits_controller.rb
@@ -5,7 +5,7 @@ module Api
     
     def create
       begin
-        credit = Credit.new(params[:credit].merge({user_id: @current_user.id}))
+        credit = Credit.new(params[:credit])
         if credit.save
           @response = {success: true, success_message: "Credit record created", credit: credit}
         else
@@ -22,7 +22,10 @@ module Api
 
   private
   def credit_params
-    params[:credit].permit(:amount, :user_id, :source)
+    if params[:credit].present?
+      params[:credit].merge!({user_id: @current_user.id})
+      return params[:credit].permit(:amount, :user_id, :source)
+    end
   end
     
 end
