3ff3870f8d157d2445e1afcb06aaebc079c40c17
Richa <bhardwaj.richa17@gmail.com>
2014-03-14 18:59:23 +0530

    fixed menu api

--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -5,9 +5,20 @@ module Api
     
     def index
       @menu = Menu.current
-      @main_items = Item.main
-      @side_items = Item.side
-      respond_with(menu: @menu, main_items: @main_items, side_items: @side_items)
+      @main_items = @menu.items.main
+      main_items_hash = ActiveSupport::JSON.decode(@main_items.to_json)
+      main_items = main_items_hash.collect do |item_hash|
+        item = Item.find_by_id(item_hash["id"])
+        item_hash.slice!("id", "name", "sku", "price", "category_id", "vendor_id").merge!({"original_url" => item.image}) if item
+      end.compact
+      @side_items = @menu.items.side
+      side_items_hash = ActiveSupport::JSON.decode(@side_items.to_json)
+      side_items = side_items_hash.collect do |item_hash|
+        item = Item.find_by_id(item_hash["id"])
+        item_hash.slice!("id", "name", "sku", "price", "category_id", "vendor_id").merge!({"original_url" => item.image}) if item
+      end.compact
+
+      respond_with(menu: @menu, main_items: main_items, side_items: side_items)
     end
   end
     
