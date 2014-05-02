65ad03a6bc1a4981a1a10eb8982224d8f7159f43
David Chua <zhchua@gmail.com>
2014-05-02 16:58:48 +0800

    Push capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,88 +1,143 @@
-require "rvm/capistrano"
-require 'capistrano/ext/multistage' 
+load 'deploy/assets'
+require "bundler/capistrano"
+require "dotenv/capistrano"
+#require 'capistrano/local_precompile'
 
-set :stages, ["staging", "production"]
-set :default_stage, "staging"
-set :keep_releases, 3
-set :repository, "git@bitbucket.org:getting-real/feedex-backend.git"
+set :application, "feedex"
+set :repository,  "git@github.com:Getting-Real/feedex-backend.git"
 
-default_run_options[:pty] = true
-set :scm, :git
-ssh_options[:forward_agent] = true
-
-set :deploy_via, :remote_cache
-set :git_enable_submodules, 1
-set :user, 'ubuntu'
+set :deploy_to, "/webapps/feedex"
+set :branch, :develop
+set :user, 'deployer'
 set :use_sudo, false
-set :deploy_to, "/home/ubuntu/feedex"
-set :default_shell, "/bin/bash -l"
+set :asset_env, "RAILS_GROUPS=assets"
+set :assets_role, :app
 
-set :test_log, "log/capistrano.test.log"
+set :rvm_type, :system
 
-namespace :deploy do
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{ current_path }/tmp/restart.txt"
-  end
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-  [:start, :stop].each do |t|
-    task t, :roles => :app do ; end
-  end
+role :web, "54.255.170.227"                          # Your HTTP server, Apache/etc
+role :app, "54.255.170.227"                          # This may be the same as your `Web` server
+role :db,  "54.255.170.227", :primary => true # This is where Rails migrations will run
 
-  desc "Deploy with migrations"
-   task :long do
-     transaction do
-       update_code
-       web.disable
-       create_symlink
-       migrate
-     end
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
 
-     restart
-     web.enable
-     cleanup
-   end
+before 'deploy:db:symlink_db', 'deploy:setup_config'
+before 'deploy:assets:precompile', 'deploy:db:symlink_db'
+after "deploy", "deploy:migrate"
+after 'deploy:update_code', 'deploy:symlink_shared'
+after 'deploy:update_code', 'deploy:setup_config'
+after 'deploy:update_code', 'deploy:db:symlink_db'
+after 'deploy:update_code', 'deploy:assets:precompile'
 
-  task :custom_symlink, :roles => :app do
-    # run "ln -s #{ shared_path }/robots.txt #{ current_path }/public/robots.txt"
-    run "ln -s #{ shared_path }/database.yml #{ current_path }/config/database.yml"
-  end
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
 
-  task :delayed_job, :roles => :app do
-    #run "RAILS_ENV=production #{current_path}/bin/delayed_job start"
-  end
-
-  after "deploy:create_symlink", "deploy:custom_symlink"
-  after "deploy:create_symlink", "deploy:delayed_job"
-  after "deploy:update", "deploy:cleanup"
-  
+# If you are using Passenger mod_rails uncomment this:
+def remote_file_exists?(full_path)
+    'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
 end
 
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(release_path, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :setup_config do
+    run "mkdir -p #{deploy_to}/shared/config"
+    run "mkdir -p #{deploy_to}/shared/log"
+#    run "chmod -Rf 0666 #{deploy_to}/shared/log/"
+    if remote_file_exists?("#{deploy_to}/shared/config/database.yml")
+      puts "database.yml exists, continuing on ..."
+    else
+      run "cd #{current_release}; cp -u #{current_release}/config/database.yml.example #{deploy_to}/shared/config/database.yml"
+    end
+  end
+  task :symlink_shared do
+    run "ln -nfs #{deploy_to}/shared/assets #{current_release}/public/assets"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 
-  task :install, :roles => :app do
-    run "cd #{release_path} && bundle install --path '#{shared_path}/bundle' --deployment --quiet --without development test"
+  namespace :db do
 
-    on_rollback do
-      if previous_release
-        run "cd #{release_path} && bundle install --path '#{shared_path}/bundle' --deployment --quiet --without development test"
-      else
-        logger.important "no previous release to rollback to, rollback of bundler:install skipped"
-      end
+    task :seed do
+      run "cd #{current_release}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+    end
+
+    desc "Symlinks the database.yml"
+    task :symlink_db, :roles => :app do
+      run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+    end
+
+    task :reset, :roles => [:web, :db] do
+      run "cd #{current_release}; bundle exec rake db:drop RAILS_ENV=#{rails_env}"
+      run "cd #{current_release}; bundle exec rake db:create RAILS_ENV=#{rails_env}"
+      run "cd #{current_release}; bundle exec rake db:migrate RAILS_ENV=#{rails_env}"
     end
   end
 
-  task :bundle_new_release, :roles => :db do
-    bundler.create_symlink
-    bundler.install
+namespace :assets do
+  task :precompile, :roles => :web, :except => { :no_release => true } do
+    begin
+      from = source.next_revision(current_revision)
+    rescue
+      err_no = true
+    end
+    if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+      puts "running the assets"
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} RAILS_GROUPS=assets assets:precompile}
+    else
+      logger.info "Skipping asset pre-compilation because there were no asset changes"
+    end
+  end
+end
+end
+def run_interactively(command, server=nil)
+    server ||= find_servers_for_task(current_task).first
+    exec %Q(ssh #{server.host} -l #{user} -t 'source /etc/profile && cd #{current_path} && #{command}')
+end
+
+namespace :rails do
+  desc "Open the rails console on one of the remote servers"
+  task :console, :roles => :app do
+    puts "You're about to enter PRODUCTION level console..."
+    puts "Please exercise caution ..."
+    run_interactively "bundle exec rails console #{rails_env}"
+  end
+
+  desc "tail production log files"
+  task :tail_logs, :roles => :app do
+    trap("INT") { puts 'Interupted'; exit 0; }
+    run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+      puts  # for an extra line break before the host name
+      puts "#{channel[:host]}: #{data}"
+      break if stream == :err
+    end
+  end
+  desc "tail printer logs"
+  task :tail_printer_logs, :roles => :app do
+    trap("INT") { puts 'Interupted'; exit 0; }
+    run "tail -f #{shared_path}/log/printer.log" do |channel, stream, data|
+      puts  # for an extra line break before the host name
+      puts "#{channel[:host]}: #{data}"
+      break if stream == :err
+    end
+  end
+  desc "tail job logs"
+  task :tail_job_logs, :roles => :app do
+    trap("INT") { puts 'Interupted'; exit 0; }
+    run "tail -f #{shared_path}/log/job.log" do |channel, stream, data|
+      puts  # for an extra line break before the host name
+      puts "#{channel[:host]}: #{data}"
+      break if stream == :err
+    end
   end
 end
 
-after "deploy:rollback:revision", "bundler:install"
-after "deploy:update_code", "bundler:bundle_new_release"
+require "rvm/capistrano"
