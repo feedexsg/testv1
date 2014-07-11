541c6cacb97cc6816d7d12768d9ad1d534f44e40
Wang Jiadong <wangjiadong1993@gmail.com>
2014-07-11 12:03:02 +0800

    updated menu js, css and html; animation in progress

--- app/assets/javascripts/web/menus.js
+++ app/assets/javascripts/web/menus.js
@@ -1,22 +1,33 @@
 var web = angular.module("web", ['ngAnimate']);
 web.controller('maincontroller', function($scope, Colonygetter, Itemgetter, Menusgetter) {
+    // pre define of varibales
     $scope.name = {}
     $scope.name.test = "wangjiadong"
     $scope.name.show = true;
+
+    $scope.name.mainindex = 0;
+    $scope.name.sideindex = 0;
+
+    // get the array of image urls
     Menusgetter.get().success(function(data) {
         $scope.name.side = []
         $scope.name.main = []
-        console.log(data)
-        $scope.name.test = data
         for (i = 0; i <= data['side_items'].length - 1; i++) {
-            $scope.name.side.push(data['side_items'][i]['original_url'])
+            $scope.name.side.push(data['side_items'][i])
         }
         for (i = 0; i <= data['main_items'].length - 1; i++) {
-            $scope.name.main.push(data['main_items'][i]['original_url'])
+            $scope.name.main.push(data['main_items'][i])
         }
-
     })
 
+    // to detect and update runtime
+    $scope.$watch('name.main', function() {
+        $scope.name.mainleng = $scope.name.main.length
+    })
+    // to detect and update runtime
+    $scope.$watch('name.side', function() {
+        $scope.name.sideleng = $scope.name.side.length
+    })
 });
 
 /*http get colony index*/
@@ -77,6 +88,7 @@ web.factory('Menusgetter', function($http) {
                     'X-Auth': 'c63c4aabc197ab6016aaad8ba2b44a76'
                 }
             })
+
         }
     }
 });
