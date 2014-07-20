7f4ae141983a1273bbecb501844190f657b4ea80
Gabriel Lim <gabriel@gettingrail.com>
2014-07-20 16:47:42 +0800

    Testing for SHA1 Signature

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,3 +1,4 @@
+require 'digest/sha1'
 class PaymentsController < ApplicationController
 	protect_from_forgery with: :null_session
 	respond_to :json
@@ -9,7 +10,39 @@ class PaymentsController < ApplicationController
 		Rails.logger.info "Trying to get merchant..."
 		Rails.logger.info params[:merchant]
 
-		render nil
+		Rails.logger.info "Getting Signature....."
+		Rails.logger.info "**** Signature *****"
+		Rails.logger.info params[:signature]
+
+		secret_key = "0db5a5b4cc2840009ff2c2c98a520582"
+		merchant = params[:merchant]
+		ref_id = params[:ref_id]
+		reference_code = params[:reference_code]
+		response_code = params[:response_code]
+		currency = params[:currency]
+		total_amount = params[:total_amount]
+		signature = params[:signature]
+		signature_algorithm = params[:signature_algorithm]
+
+		data_to_be_hashed = secret_key + merchant + ref_id + reference_code + response_code + currency + total_amount
+
+		utf_string = data_to_be_hashed.encode('utf-8')
+		check_signature = Digest::SHA1.hexdigest utf_string
+
+		Rails.logger.info "**** Check Signature *****"
+		Rails.logger.info check_signature
+
+
+		if (params[:signature] == check_signature)
+			Rails.logger.info "DING DING DING MATCH!"
+
+		else
+			Rails.logger.info "BLEAH DO NOT MATCH!"
+		end
+
+
+
+		render :nothing => true
 
 
 
