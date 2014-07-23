5c9921099d5b62694b6d1edcb7318c31f9314d84
Gabriel Lim <gabriel@gettingrail.com>
2014-07-23 16:19:43 +0800

    Logic error fixed for login and confirmation

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -4,7 +4,12 @@ class MenusController < ApplicationController
   include SessionsHelper
   before_action :set_cart, only: [:index]
   before_action :signed_in_user, only: [:index]
+  before_filter :detect_browser
+
   def index
+    Rails.logger.info "******************@**@**@*  User is using...."
+    Rails.logger.info @browser
+    Rails.logger.info "*****"
   	@menu = Menu.last # Menu.current
   	@main_items = @menu.items.main
       main_items_hash = ActiveSupport::JSON.decode(@main_items.to_json)
@@ -28,6 +33,17 @@ class MenusController < ApplicationController
   private
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
+
+  def detect_browser
+    @browser = "desktop"
+    agent = request.headers["HTTP_USER_AGENT"].downcase
+    MOBILE_BROWSERS.each do |m|
+      @browser =  "mobile_application" if agent.match(m)
+      return
+    end
   end
   
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,20 +6,22 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
-		if user.confirmed_at.nil?
-			flash.now[:error] = 'Please confirm your email address first'
-			render 'new'
-		else
+		
 			if user && user.authenticate(params[:session][:password])
-				# Sign in and redirect to menu
-				sign_in user
-				redirect_to menus_url
+				if user.confirmed_at.nil?
+					flash.now[:error] = 'Please confirm your email address first'
+					render 'new'
+				else
+					# Sign in and redirect to menu
+					sign_in user
+					redirect_to menus_url
+				end
 			else
 				# Create an error message
 				flash.now[:error] = 'Invalid email/password combination'
 				render 'new'
 			end
-		end
+		
 	end
 
 	def destroy
