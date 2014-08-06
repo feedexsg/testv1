7d925dac663864730952f0014124ddc9adefc4f3
Gabriel Lim <gabriel@gettingrail.com>
2014-08-06 23:26:43 +0800

    params require for password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,11 +13,18 @@ class PasswordResetsController < ApplicationController
 		@user = User.find_by_reset_password_token!(params[:id])
 		if @user.reset_password_sent_at < 2.hours.ago
 			redirect_to new_password_reset_path, :alert => "Password reset has expired."
-		elsif @user.update_attributes(params[:user])
+		elsif @user.update_attributes(user_params)
 			redirect_to menus_url, :notice => "Password has been reset!"
 		else
 			render :edit
 		end
 	end
+
+	private
+
+	  def user_params
+	  	params.require(:user).permit(:name, :email, :mobile, :password, :password_confirmation,
+	                                 :colony_id, credits_attributes: [:amount, :source])
+	  end
 
 end
