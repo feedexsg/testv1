081e48ec97b696bcb88f242f2fe33ef5fd8e7443
Gabriel Lim <gabriel@gettingrail.com>
2014-08-06 23:37:26 +0800

    changed flash for users sign in

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,7 +14,7 @@ class PasswordResetsController < ApplicationController
 		if @user.reset_password_sent_at < 2.hours.ago
 			redirect_to new_password_reset_path, :alert => "Password reset has expired."
 		elsif @user.update_attributes(user_params)
-			redirect_to menus_url, :notice => "Password has been reset!"
+			redirect_to signin_path, :notice => "Password has been reset!"
 		else
 			render :edit
 		end
