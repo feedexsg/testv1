ceeab657afdbc5e15df45b262d15b776da4bfc1a
Charlton <hello@feedex.sg>
2014-10-30 22:20:08 +0800

    price == 0.0

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -53,7 +53,7 @@ class MenusController < ApplicationController
       @current_user ||= User.find_by(remember_token: remember_token)
       @total_price = 0.0
       @cart.line_items.each do |item|
-        if Item.find(item.side_id).price == 0 
+        if Item.find(item.side_id).price == 0.0 
           @total_price += Item.find(item.main_id).price
         else 
         @total_price += 0.9 * Item.find(item.main_id).price 
