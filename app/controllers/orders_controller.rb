59281a11649d4db12ab39beced57e120841eae10
Charlton <hello@feedex.sg>
2014-10-31 22:05:08 +0800

    Edited orders_controller L22

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,10 +19,13 @@ class OrdersController < ApplicationController
 		item_sets.each do |i|
 			main_item = Item.find_by_id(i["main_id"])
 			side_item = Item.find_by_id(i["side_id"])
-			
+			if Item.find(item.side_id).price == 0
 			total_amt += main_item.price.to_f if main_item
           	total_amt += side_item.price.to_f if side_item
-
+          else 
+          	total_amt += main_item.price.to_f if main_item
+          	total_amt += side_item.price.to_f if side_item
+          end
           	main_count_hash = {}
           	main_count_hash[:item_id] = main_item.id
           	main_count_hash[:item] = main_item
